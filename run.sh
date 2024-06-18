e4c8c62b818d8865ace58b519cf624a585d595ad
Diletta Calá Impirotta <g.calaimpirotta@gmail.com>
2024-06-18 11:06:00 +0200

    Added .env variables for tolken

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -a # automatically export all variables
+source .env
+set +a
 
 python3 ./fill_template.py ./template.txt data.csv
 
 echo Copy the .pdf files to the folder of .json files.
 read -rsp $'Press enter to continue...\n'
 
-TOKEN=INSERT_YOUR_ZENODO_TOKEN_HERE
+TOKEN=$ACCESS_TOKEN_SANDBOX
 python3 ./upload_to_zenodo.py $TOKEN .
